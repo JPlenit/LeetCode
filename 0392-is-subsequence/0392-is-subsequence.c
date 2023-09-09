7d287a90d4f6b7f573d27f01d4bde08e085986e6
JPlenit <133968501+JPlenit@users.noreply.github.com>
2023-09-09 14:48:38 +0900

    Time: 3 ms (36.49%), Space: 5.6 MB (30.11%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.c
@@ -0,0 +1,11 @@
+bool isSubsequence(char * s, char * t){
+    int tSize = strlen(t);
+    int sSize = strlen(s);
+    int j, k = 0;
+    if (sSize == 0) return true;
+    for (int i = 0; i < tSize; i++) {
+        if (t[i] == s[k]) k++;
+        if (sSize == k) return true;
+    }
+    return false;
+}
