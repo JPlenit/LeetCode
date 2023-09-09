90755977481ecd9c31e778ea7217ffd64006c5d1
JPlenit <133968501+JPlenit@users.noreply.github.com>
2023-09-09 10:39:48 +0900

    Time: 61 ms (90.93%), Space: 6.4 MB (27.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int value;
+    int j;
+    int* array = (int*)malloc(sizeof(int) * 2);
+    *returnSize = 2;
+    for (int i = 0; i < numsSize; i++) {
+        value = target - nums[i];
+        for (j = i + 1; j < numsSize; j++) {
+            if (nums[j] == value) {
+                array[0] = i;
+                array[1] = j;
+                return array;
+            }
+        }
+    }
+    return array;
+}
