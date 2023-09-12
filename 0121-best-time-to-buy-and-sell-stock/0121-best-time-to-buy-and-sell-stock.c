f72a0253bdd301ee96c6833a51044d5527612de8
JPlenit <133968501+JPlenit@users.noreply.github.com>
2023-09-12 18:12:36 +0900

    Time: 155 ms (20.03%), Space: 13 MB (39.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -1,15 +1,14 @@
 int maxProfit(int* prices, int pricesSize){
-    //int min, max;
-    //min = prices[0]; max = prices[0];
-    int p = 0, temp1 = 0, temp2 = 1;
+    
+    int p = 0, min = 0, max = 1;
 
     for (int i = 1; i < pricesSize; i++) {
         if (p < prices[i]) {
-            for (int j = temp1; j < i; j++) {
-            if (p < prices[i] - prices[j]) {
-                p = prices[i] - prices[j];
-                temp1 = j;
-                temp2 = i;
+            for (int j = min; j < i; j++) {
+                if (p < prices[i] - prices[j]) {
+                    p = prices[i] - prices[j];
+                    min = j;
+                    max = i;
                 }
             }
         }
