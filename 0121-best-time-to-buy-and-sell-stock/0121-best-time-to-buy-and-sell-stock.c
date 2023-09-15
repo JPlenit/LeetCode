d44b3230a0b7817966c54ade22fd585844cc5768
JPlenit <133968501+JPlenit@users.noreply.github.com>
2023-09-15 15:03:18 +0900

    Time: 88 ms (95.81%), Space: 13.5 MB (9.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -1,35 +1,17 @@
-// struct linkedlist{
-//     int index;
-//     struct linkedlist* node;
-// }
-// typedef struct linkedlist link;
-
 int maxProfit(int* prices, int pricesSize){
 
-    int p = 0, min = 0, max = 1;
+    int p = 0, min = 0, max = 0;
 
 
     for (int i = 1; i < pricesSize; i++) {
         if (prices[i] <= prices[min]) {
             min = i;
             max = i;
-            if(max > min && prices[max] - prices[min] > p) p = prices[max] - prices[min];
         }
         else if (prices[i] >= prices[max]) {
             max = i;
             if(max > min && prices[max] - prices[min] > p) p = prices[max] - prices[min];
         }
     }
-    // for (int i = 1; i < pricesSize; i++) {
-    //     if (prices[max] <= prices[i]) {
-    //         for (int j = min; j < i; j++) {
-    //         if (p < prices[i] - prices[j]) {
-    //             p = prices[i] - prices[j];
-    //             min = j;
-    //             max = i;
-    //             }
-    //         }
-    //     }
-    // }
     return p;
 }
