fb06210e08b98b5445af8a5a2bc1879b16c9bd32
JPlenit <133968501+JPlenit@users.noreply.github.com>
2023-09-16 13:25:15 +0900

    Time: 18 ms (51.93%), Space: 7.3 MB (8.32%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.c
+++ 0724-find-pivot-index/0724-find-pivot-index.c
@@ -1,11 +1,17 @@
 int pivotIndex(int* nums, int numsSize){
-    int left = 0, right = numsSize - 1;
     int leftSum = 0, rightSum = 0;
-    for (int i = 0; i < numsSize; i++) {
-        for (int j = 0; j < i; j++) leftSum += nums[j];
-        for (int j = i + 1; j < numsSize; j++) rightSum += nums[j];
+    // for (int i = 0; i < numsSize; i++) {
+    //     for (int j = 0; j < i; j++) leftSum += nums[j];
+    //     for (int j = i + 1; j < numsSize; j++) rightSum += nums[j];
+    //     if (leftSum == rightSum) return i;
+    //     leftSum = 0; rightSum = 0;
+    // }
+    for (int j = 1; j < numsSize; j++) rightSum += nums[j];
+    for (int i = 0; i < numsSize - 1; i++) {
         if (leftSum == rightSum) return i;
-        leftSum = 0; rightSum = 0;
+        leftSum += nums[i];
+        rightSum = rightSum - nums[i + 1];
     }
+    if (leftSum == rightSum) return numsSize - 1;
     return -1;
 }
